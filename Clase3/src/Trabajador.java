6953cb4fbc5b04deca3e6063cc7cc0ddf794451d
Jhon Cano <jhoncano@MacBook-Pro-de-jhoncano.local>
2021-08-24 18:44:44 -0500

    2120

--- Clase3/src/Trabajador.java
+++ Clase3/src/Trabajador.java
@@ -1,3 +1,28 @@
 public class Trabajador {
+
+    private int id;
+    private String nombre;
+    private String apellido;
     
+    public Trabajador(int id, String nombre, String apellido) {
+        this.id = id;
+        this.nombre = nombre;
+        this.apellido = apellido;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public double pagar(){
+        return 0.0;
+    }
 }
