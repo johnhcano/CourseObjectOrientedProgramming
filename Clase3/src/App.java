b9ec860c878ff4813760c252e1865a2a982d0a68
Jhon Cano <jhoncano@MacBook-Pro-de-jhoncano.local>
2021-08-24 19:26:36 -0500

    2120

--- Clase3/src/App.java
+++ Clase3/src/App.java
@@ -1,5 +1,18 @@
 public class App {
     public static void main(String[] args) {
         
+        Trabajador[] trabajadores = new Trabajador[6];
+
+        trabajadores[0] = new Consultor(1, "Peter", "Maison", 4);
+        trabajadores[1] = new Empleado(2, "Byron", "Cano", 10);
+        trabajadores[2] = new Empleado(3, "Harvin", "Beltran", 33);
+        trabajadores[3] = new Consultor(4, "Patricia", "Girón", 11.2);
+        trabajadores[4] = new Consultor(5, "Blanca", "Girón", 3.4);
+        trabajadores[5] = new Empleado(6, "Mercedes", "Fonseca", 15);
+
+        for (Trabajador t: trabajadores){
+            System.out.println(t.getNombre() + "-" + t.getClass() + " Gana: $" + t.pagar());
+        }
+        
     }
 }
