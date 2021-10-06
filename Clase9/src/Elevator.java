2950c9bd6ca2482c0f5187191e70dea0e01d9b5d
Jhon Cano <jhoncano@MacBook-Pro-de-jhoncano.local>
2021-10-05 19:18:45 -0500

    2120

---
+++ Clase9/src/Elevator.java
@@ -0,0 +1,38 @@
+public class Elevator {
+
+    private boolean direction;
+    private int currentFloor;
+    
+    public Elevator(boolean direction, int currentFloor) {
+        this.direction = direction;
+        this.currentFloor = currentFloor;
+    }
+
+    public boolean isDirection() {
+        return direction;
+    }
+
+    public int getCurrentFloor() {
+        return currentFloor;
+    }
+
+    public void move(){
+        System.out.println("move");
+    }
+
+    public void stop(){
+        System.out.println("stop");
+    }
+    
+    public void status(){
+        System.out.println("status");
+    }
+
+    @Override
+    public String toString() {
+        return "Elevator [currentFloor=" + currentFloor + ", direction=" + direction + "]";
+    }
+
+
+
+}
