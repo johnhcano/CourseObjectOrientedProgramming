2a779b324608fe98adadbd22ff7d161138c73b2b
Jhon Cano <jhoncano@MacBook-Pro-de-jhoncano.local>
2021-10-05 19:35:46 -0500

    2120

--- Clase9/src/Button.java
+++ Clase9/src/Button.java
@@ -1,3 +1,32 @@
 public class Button {
     
+    private boolean illuminate = false;
+
+    public Button(boolean illuminate) {
+        this.illuminate = illuminate;
+    }
+
+    public boolean isIlluminate() {
+        return illuminate;
+    }
+
+    public void illuminate(){
+        this.illuminate = true;
+    }
+
+    public void cancelIlluminate(){
+        this.illuminate = false;
+    }
+
+    public void status(){
+        System.out.println("status");
+    }
+
+    @Override
+    public String toString() {
+        return "Button [illuminate=" + illuminate + "]";
+    }
+
+    
+    
 }
