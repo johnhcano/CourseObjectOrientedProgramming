fe5be42b71d2f15f40ac7ff247c53c290e53c18f
Jhon Cano <jhoncano@MacBook-Pro-de-jhoncano.local>
2021-10-05 19:30:10 -0500

    2120

--- Clase9/src/Door.java
+++ Clase9/src/Door.java
@@ -1,3 +1,28 @@
 public class Door {
     
+    private boolean close = true;
+
+    public Door(boolean close) {
+        this.close = close;
+    }
+
+    public boolean isClose() {
+        return close;
+    }
+
+    public void close(){
+        this.close = true;
+    }
+
+    public void open(){
+        this.close = false;
+    }
+
+    @Override
+    public String toString() {
+        return "Door [close=" + close + "]";
+    }
+
+    
+
 }
