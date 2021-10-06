da129a0f94a14433a779e814feaa181c2f30c1ec
Jhon Cano <jhoncano@MacBook-Pro-de-jhoncano.local>
2021-10-05 19:40:16 -0500

    2120

--- Clase9/src/ElevatorButton.java
+++ Clase9/src/ElevatorButton.java
@@ -1,3 +1,19 @@
-public class ElevatorButton {
+public class ElevatorButton extends Button {
+
+    private int floorNum;
+
+    public ElevatorButton(boolean illuminate, int floorNum) {
+        super(illuminate);
+        this.floorNum = floorNum;
+    }
+
+    public int getFloorNum() {
+        return floorNum;
+    }
+
+    @Override
+    public String toString() {
+        return "ElevatorButton [floorNum=" + floorNum + "]";
+    }   
     
 }
--- Clase9/src/FloorButton.java
+++ Clase9/src/FloorButton.java
@@ -1,3 +1,25 @@
-public class FloorButton {
+public class FloorButton extends Button {
+    
+    private int floorNum;
+    private boolean direction;
+    
+    public FloorButton(boolean illuminate, int floorNum, boolean direction) {
+        super(illuminate);
+        this.floorNum = floorNum;
+        this.direction = direction;
+    }
+
+    public int getFloorNum() {
+        return floorNum;
+    }
+
+    public boolean isDirection() {
+        return direction;
+    }
+
+    @Override
+    public String toString() {
+        return "FloorButton [direction=" + direction + ", floorNum=" + floorNum + "]";
+    }
     
 }
