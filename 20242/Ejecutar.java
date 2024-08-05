4504da6b9155b6be6dcae8d979768724b8eb2cd2
JhonHCanoB <johnhcano@gmail.com>
2024-08-05 23:59:30 +0000

    Se modificó el texto

--- 20242/Ejecutar.java
+++ 20242/Ejecutar.java
@@ -1,5 +1,5 @@
 public class Ejecutar {
     public static void main(String[] args){
-        System.out.println("Programación Orientada a Objetos");
+        System.out.println("Programación OO");
     }
 }
