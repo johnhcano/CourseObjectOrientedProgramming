54f6814ba4e5a4261033d94176abd7b8dd64c985
JhonHCanoB <johnhcano@gmail.com>
2024-08-06 01:32:40 +0000

    Ejemplo Concepto Clase

---
+++ 20242/Perro.java
@@ -0,0 +1,21 @@
+public class Perro {
+    
+    //Atributos
+    private String color;
+    private double peso;
+    private char genero;
+
+    //Métodos
+    public boolean saltar(double p){
+        return true;
+    }
+
+    public void comer(){
+        System.out.println("Comiendo..");
+    }
+
+    public String jugar(){
+        return "Jugando";
+    }
+    
+}
