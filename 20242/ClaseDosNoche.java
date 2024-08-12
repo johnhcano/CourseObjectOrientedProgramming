7aa29d35f506c7aa8f9b82a7bdc85de99df6bc83
JhonHCanoB <johnhcano@gmail.com>
2024-08-12 23:48:59 +0000

    Expresiones en JAVA

--- 20242/ClaseDosNoche.java
+++ 20242/ClaseDosNoche.java
@@ -9,5 +9,37 @@ public class ClaseDosNoche {
          * líneas
          */
 
+        /*
+         * Asignación (=)
+         * Comparación (==)
+         * a = 5
+         * b= 10
+         */
+
+        /* 
+         * Tipos de Datos
+         * 
+         * Numéricos -> Enteros (int, long), Reales (float, double)
+         * Lógicos -> Booleanos (boolean)
+         * Texto -> Cadenas (String), caracter (char)
+         * 
+         * Palabras reservadas del lenguaje JAVA
+         * public, float, int, void, class, if, for, break, switch, while, return, String, ...
+         * 
+         * 
+         * JAVA todas las variables deben ser declaradas antes de trabajar con ellas.
+         * 
+         */
+
+        int a = 5;
+        int b = 10;
+        int c = a + b;
+        //Para concatenar cadenas y variables se utiliza el operador (+)
+        System.out.println("La suma entre " + a + " y " + b + " es: " + c);
+
+        
+
+        
+
     }
 }
