7537430d893e000e36737619db1e090c583521a3
JhonHCanoB <johnhcano@gmail.com>
2024-08-12 23:32:25 +0000

    Expresiones en JAVA

--- 20242/ClaseDosNoche.java
+++ 20242/ClaseDosNoche.java
@@ -1,7 +1,13 @@
 public class ClaseDosNoche {
     public static void main(String[] args) {
         
-        
-        
+        //Comentarios de una sola línea
+
+        /*
+         * Comentarios de
+         * varias
+         * líneas
+         */
+
     }
 }
