f8e1e5d8d7c59f05bee9f7213ca309725195d40a
JhonHCanoB <johnhcano@gmail.com>
2024-08-12 14:15:24 +0000

    Ejercicios - Expresiones

--- 20242/Ejecutar.java
+++ 20242/Ejecutar.java
@@ -29,9 +29,9 @@ public class Ejecutar {
 
         /*
          * Ejercicios: 
-         * 1. Diseñe un algoritmo que calcule el área de un cuadrado, teniendo uno de sus  lados.
-         * A = b^2 
-         * 2. Diseñe un algoritmo que calcule el área de un círculo. A = π r^2 
+         * 1. Diseñe un algoritmo que calcule el área de un cuadrado, 
+         *    teniendo uno de sus lados. areaCuadrado = b^2 
+         * 2. Diseñe un algoritmo que calcule el área de un círculo. areaCirculo = π r^2 
          * 3. Diseñe un algoritmo que convierta de grados centígrados a grados fahrenheit y  viceversa 
          * 4. Diseñe un algoritmo para calcular el salario de un empleado. 
          *    salarioNeto = (salarioBruto - deducciones) + comisión 
@@ -39,6 +39,21 @@ public class Ejecutar {
          * 6. Diseñe un algoritmo para calcular el pago de un parqueadero por horas. 
          *    Valor de  la hora $2000 también se debe discriminar el IVA del 20% que se paga. 
          */
+
+         /* Solución ejercicio 1.
+          * Entrada: lado del cuadrado -> b
+          * Proceso: areaCuadrado = b * b
+          * Salida: areaCuadrado
+        */
+        //inicializar las variables
+        int b = 0; 
+        int areaCuadrado = 0;  
+        
+        b = 2; // Asignación de la variable
+        areaCuadrado = b * b; // Fórmula para hallar el área de un cuadrado
+        System.out.println("Área del Cuadrado = " + areaCuadrado); //Mensaje de salida
+
+        //Para concatenar texto con variables se utiliza el signo (+)
 
 
     }
