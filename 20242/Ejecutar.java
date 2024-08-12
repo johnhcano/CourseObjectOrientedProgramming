40ddd2af481af74fa3b1b1413c9e67987c70b3ea
JhonHCanoB <johnhcano@gmail.com>
2024-08-12 13:00:08 +0000

    Introducción a JAVA

--- 20242/Ejecutar.java
+++ 20242/Ejecutar.java
@@ -1,5 +1,27 @@
 public class Ejecutar {
     public static void main(String[] args){
-        System.out.println("Programación OO");
+
+        //Comentario de una sola línea
+
+        /*
+         * Comentario de
+         * varias 
+         * líneas
+         */
+
+        //JAVA la asignación es con el = (igual)
+        // a = 5 -> a la variable (a) le asignamos un 5
+        // b = 10 --> a la variable (b) le asignamos un 10
+        // nombre = "Cristiano" --> a la variable nombre le asignamos el texto "Cristiano"
+        // == (igual igual, doble igual) --> es de comparación
+        // Tener cuidado que las variables tienen un tipo de dato
+
+        //https://www.dit.upm.es/~pepe/libros/vademecum.pdf
+
+        //https://docs.oracle.com/javase/tutorial/tutorialLearningPaths.html
+        
+
+
+
     }
 }
