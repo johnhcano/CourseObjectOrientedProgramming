a54d14afdc97d80579248e3325e5c343b5410da7
JhonHCanoB <johnhcano@gmail.com>
2024-08-26 14:45:13 +0000

    POO

--- 20242/ClaseObjetos_Diurno/Ejecutar.java
+++ 20242/ClaseObjetos_Diurno/Ejecutar.java
@@ -3,11 +3,7 @@ public class Ejecutar {
     public static void main(String[] args){
         //Creación del objeto
         Marcador objMarcador1 = new Marcador("edding", "rojo", 1.2, true);
-
-        
-
-
-
+        System.out.println(objMarcador1.toString());
     }
     
 }
