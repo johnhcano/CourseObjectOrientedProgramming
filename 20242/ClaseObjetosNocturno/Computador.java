5328d650a237da2742ef420e901978f6aa02441b
JhonHCanoB <johnhcano@gmail.com>
2024-08-27 00:55:30 +0000

    POO

--- 20242/ClaseObjetosNocturno/Computador.java
+++ 20242/ClaseObjetosNocturno/Computador.java
@@ -1,15 +1,15 @@
 //package ClaseObjetosNocturno;
 public class Computador {
 
-    //Atributos de la clase
+    // Atributos de la clase
     private String marca;
     private String color;
     private String modelo;
     private int serie;
     private double pulgadasPantalla;
-    
-    //Método Constructor de la Clase
-    public Computador(String marca, String color, String modelo, int serie, double pulgadasPantalla){
+
+    // Método Constructor de la Clase
+    public Computador(String marca, String color, String modelo, int serie, double pulgadasPantalla) {
         this.marca = marca;
         this.color = color;
         this.modelo = modelo;
@@ -17,24 +17,25 @@ public class Computador {
         this.pulgadasPantalla = pulgadasPantalla;
     }
 
-    //Métodos de la clase
-    public boolean encender(){
-        return true;
-    }
-    
-    public boolean apagar(){
+    // Métodos de la clase
+    public boolean encender() {
         return true;
     }
 
-    public void reubicar(String oficina){
+    public boolean apagar() {
+        return true;
+    }
+
+    public void reubicar(String oficina) {
         System.out.println("El computador de marca " + marca + " se reubicó en la oficina " + oficina);
     }
 
-    public void asignar(String trabajador){
+    public void asignar(String trabajador) {
         System.out.println("El computador con serie " + serie + " fue asignado a: " + trabajador);
     }
 
-    public String toString(){
-        return "Computador { marca: " + marca + " color: " + color + " modelo " + modelo +" serie " + serie + " pulgadas " + pulgadasPantalla+ " }";
+    public String toString() {
+        return "Computador { marca: " + marca + " color: " + color + " modelo: " + modelo + " serie: " + serie
+                + " pulgadas: " + pulgadasPantalla + " }";
     }
 }
