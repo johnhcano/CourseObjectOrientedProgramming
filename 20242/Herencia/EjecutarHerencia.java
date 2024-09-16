e167d51eb56e917aabffe5415cf86561e1255dc7
JhonHCanoB <johnhcano@gmail.com>
2024-09-16 14:48:51 +0000

    Herencia

--- 20242/Herencia/EjecutarHerencia.java
+++ 20242/Herencia/EjecutarHerencia.java
@@ -8,11 +8,22 @@ public class EjecutarHerencia {
         Trabajador objT3 = new Operario(3, "Byron", "Dulcería", 200);
         Trabajador objT4 = new Operario(4, "Harvin", "Comestibles", 230);
 
-        System.out.println("Salario Vendedor: " + objT1.salario());
-        System.out.println("Salario Operario: " + objT3.salario());
+        //System.out.println("Salario Vendedor: " + objT1.salario());
+        //System.out.println("Salario Operario: " + objT3.salario());
 
         System.out.println(objT1 instanceof Vendedor); // prints true
         System.out.println(objT1 instanceof Trabajador); // prints true
+
+        Trabajador[] t = new Trabajador[4];
+        t[0] = objT1;
+        t[1] = objT3;
+        t[2] = objT2;
+        t[3] = objT4;
+
+        for(int i = 0; i < t.length; i++){
+            System.out.println(t[i].getClass() + " - " + t[i].getNombre() + " " + t[i].salario());
+        }
+        
         
     }
 } 
