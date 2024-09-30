0aa328ff3f0ca0ca9f2c78286f38cf473ff65402
JhonHCanoB <johnhcano@gmail.com>
2024-09-30 13:52:27 +0000

    ComposicionAgregacion

--- 20242/ClaseObjetos_Diurno/ComposicionAgregacion/Cliente.java
+++ 20242/ClaseObjetos_Diurno/ComposicionAgregacion/Cliente.java
@@ -1,3 +1,44 @@
 public class Cliente {
+
+    private long cedula;
+    private String nombre;
+    private String apellido;
     
+    public Cliente(long cedula, String nombre, String apellido) {
+        this.cedula = cedula;
+        this.nombre = nombre;
+        this.apellido = apellido;
+    }
+
+    public long getCedula() {
+        return cedula;
+    }
+
+    public void setCedula(long cedula) {
+        this.cedula = cedula;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    @Override
+    public String toString() {
+        return "Cliente [cedula=" + cedula + ", nombre=" + nombre + ", apellido=" + apellido + "]";
+    }
+    
+    
+
 }
