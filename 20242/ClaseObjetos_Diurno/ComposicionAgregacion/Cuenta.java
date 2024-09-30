ffc15ff597925b2d2ca1898ffce97bd8eb28e262
JhonHCanoB <johnhcano@gmail.com>
2024-09-30 14:03:20 +0000

    ComposicionAgregacion

--- 20242/ClaseObjetos_Diurno/ComposicionAgregacion/Cuenta.java
+++ 20242/ClaseObjetos_Diurno/ComposicionAgregacion/Cuenta.java
@@ -1,3 +1,42 @@
 public class Cuenta {
     
+    private String nroCuenta;
+    private String tipoCuenta;
+    private double saldo;
+    
+    public Cuenta(String nroCuenta, String tipoCuenta, double saldo) {
+        this.nroCuenta = nroCuenta;
+        this.tipoCuenta = tipoCuenta;
+        this.saldo = saldo;
+    }
+
+    public String getNroCuenta() {
+        return nroCuenta;
+    }
+
+    public void setNroCuenta(String nroCuenta) {
+        this.nroCuenta = nroCuenta;
+    }
+
+    public String getTipoCuenta() {
+        return tipoCuenta;
+    }
+
+    public void setTipoCuenta(String tipoCuenta) {
+        this.tipoCuenta = tipoCuenta;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+
+    @Override
+    public String toString() {
+        return "Cuenta [nroCuenta=" + nroCuenta + ", tipoCuenta=" + tipoCuenta + ", saldo=" + saldo + "]";
+    }
+
 }
