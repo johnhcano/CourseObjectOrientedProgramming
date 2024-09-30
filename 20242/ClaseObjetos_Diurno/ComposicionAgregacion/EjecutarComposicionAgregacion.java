8da2adde8c21af13c0134f840c90d27cb58f868c
JhonHCanoB <johnhcano@gmail.com>
2024-09-30 14:10:44 +0000

    ComposicionAgregacion

---
+++ 20242/ClaseObjetos_Diurno/ComposicionAgregacion/EjecutarComposicionAgregacion.java
@@ -0,0 +1,12 @@
+public class EjecutarComposicionAgregacion {
+    public static void main(String[] args) {
+        
+        Cliente objCliente1 = new Cliente(1008909765, "Pipe", "Perez");
+
+        Cuenta objCuenta1 = new Cuenta("709876", "Ahorros", 0.0);
+
+        
+
+
+    }
+}
