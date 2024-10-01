7f07d5a12ecaef346ee1833b60e17b37f4bd3821
JhonHCanoB <johnhcano@gmail.com>
2024-10-01 01:21:39 +0000

    Creando los objetos en la clase EjecutarComposicionAgregacion

--- 20242/ClaseObjetosNocturno/ComposicionAgregacion/EjecutarComposicionAgregacion.java
+++ 20242/ClaseObjetosNocturno/ComposicionAgregacion/EjecutarComposicionAgregacion.java
@@ -7,5 +7,8 @@ public class EjecutarComposicionAgregacion {
         LocalDateTime fechaHoraEspecifica = LocalDateTime.of(2024, 10, 15, 10, 30);
         System.out.println("Fecha y hora específica: " + fechaHoraEspecifica);
         Cita objCita1 = new Cita(fechaHoraEspecifica, "Oftalmólogo");
+
+        HistoriaClinica objHistoriaClinica;
+        objHistoriaClinica = new HistoriaClinica("Me duele el ojo", "+0.50", "hialuronato");
     }
 }
