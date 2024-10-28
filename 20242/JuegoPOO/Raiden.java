fdaa3aba8581fef974c1645a0c05ee17a482dc9a
JhonHCanoB <johnhcano@gmail.com>
2024-10-28 14:12:20 +0000

    Creación de la Clase Raiden que extiende de Personaje

---
+++ 20242/JuegoPOO/Raiden.java
@@ -0,0 +1,9 @@
+public class Raiden extends Personaje {
+    
+    private int puntosDeVida;
+
+    public Raiden(String nombre){
+        super(nombre);
+        this.puntosDeVida = 120; // Todos los Raiden comienzan con 120 puntos de vida
+    }
+}
