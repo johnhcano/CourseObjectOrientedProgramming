d4ef07963a085225419388f1cf099c1e6898e9b2
JhonHCanoB <johnhcano@gmail.com>
2025-02-10 13:33:00 +0000

    Variables

---
+++ 20251/Clase2/Variables.java
@@ -0,0 +1,27 @@
+public class Variables {
+    public static void main(String[] args) {
+        
+        //Declaración de una variable de tipo entero.
+        int edad = 25;
+        
+        //Declaración de una variable de tipo real.
+        double altura = 1.75;
+        
+        //Declaración de una variable de Cadena.
+        String nombre = "Juan";
+        
+        //Declaración de una variable de tipo boolean.
+        boolean esEstudiante = true;
+        
+        /*
+        Con la instrucción System.out.println se mostraran
+        las variables y cada una de ellas estará concatenada con el signo (+)
+        y la leyenda. 
+        */
+
+        System.out.println("Nombre: " + nombre + ", Edad: " + edad + ", Altura: " + altura);
+        System.out.println("Estudiante: " + esEstudiante);
+
+
+    }
+}
