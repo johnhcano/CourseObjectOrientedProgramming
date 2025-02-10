6e2fea4cc492d7fed77cbe9a3e32fdc5a531fdbb
JhonHCanoB <johnhcano@gmail.com>
2025-02-10 13:49:14 +0000

    Condicional simple

--- 20251/Clase2/Condicionales.java
+++ 20251/Clase2/Condicionales.java
@@ -1,3 +1,11 @@
 public class Condicionales {
-    
+    public static void main(String[] args) {
+        int numero = -5;
+        
+        if(numero >= 0){
+            System.out.println("El número es positivo");
+        }else{
+            System.out.println("El número es negativo");
+        }
+    }
 }
