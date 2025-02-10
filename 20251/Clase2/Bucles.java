8e2b57c0059fe57b0f2b7dc6cbd6483b5d89a09e
JhonHCanoB <johnhcano@gmail.com>
2025-02-10 14:55:56 +0000

    Bucles

---
+++ 20251/Clase2/Bucles.java
@@ -0,0 +1,19 @@
+public class Bucles {
+    public static void main (String[] args){
+
+        //for
+        for(int i = 1; i <= 5; i++){
+            System.out.println(i + ": Programación Orientada a Objetos");
+        }
+
+        System.out.println("------------------------");
+
+        //while
+        int j = 1;
+        while(j <= 5){
+            System.out.println(j + ": POO");
+            j++;
+        }
+
+    }
+}
