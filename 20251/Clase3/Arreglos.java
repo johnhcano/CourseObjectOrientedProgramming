15f692ed855cb26e1964e2fc121aa0acd526f3ac
JhonHCanoB <johnhcano@gmail.com>
2025-02-17 13:02:57 +0000

    Recorrido del arreglo

--- 20251/Clase3/Arreglos.java
+++ 20251/Clase3/Arreglos.java
@@ -1,15 +1,21 @@
 public class Arreglos {
-    public static void main (String[] args){
+    public static void main(String[] args) {
 
         /*
          * Primera forma de declarar un arreglo:
          * int[] a = {8, 3, 10, 22, 1, 9};
          * 
          * Segunda forma de declarar un arreglo:
-         * int[] a = new int[6]; 
+         * int[] a = new int[6];
          * El número hace referencia al tamaño del arreglo
          * tendremos que llenar el arreglo.
          */
-    }
 
+        int[] a = { 8, 3, 10, 22, 1, 9 };
+
+        for (int i = 0; i < a.length; i++) {
+            System.out.println("a[" + i + "]=" + a[i]);
+        }
+        
+    }
 }
