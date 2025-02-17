123d2f35e8d1796a5d69e6dbb6b4a975ed20dfe1
JhonHCanoB <johnhcano@gmail.com>
2025-02-17 12:53:19 +0000

    Arreglos en java

---
+++ 20251/Clase3/Arreglos.java
@@ -0,0 +1,15 @@
+public class Arreglos {
+    public static void main (String[] args){
+
+        /*
+         * Primera forma de declarar un arreglo:
+         * int[] a = {8, 3, 10, 22, 1, 9};
+         * 
+         * Segunda forma de declarar un arreglo:
+         * int[] a = new int[6]; 
+         * El número hace referencia al tamaño del arreglo
+         * tendremos que llenar el arreglo.
+         */
+    }
+
+}
