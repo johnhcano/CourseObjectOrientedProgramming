7329137ef896a944ec5e70f1272e5981ae233281
JhonHCanoB <johnhcano@gmail.com>
2025-03-17 13:50:30 +0000

    creación del método toString

--- 20251 Diurno/Clase7/Estudiante.java
+++ 20251 Diurno/Clase7/Estudiante.java
@@ -1,14 +1,14 @@
 public class Estudiante {
-    //Atributos
+    // Atributos
     private int id;
     private String nombre;
     private String apellido;
     private int edad;
     private String facultad;
 
-    //Constructor de la clase: permite inicializar la clase
-    //this --> autoreferencia a los atributos de la clase
-    public Estudiante(int id, String nombre, String apellido, int edad, String facultad){
+    // Constructor de la clase: permite inicializar la clase
+    // this --> autoreferencia a los atributos de la clase
+    public Estudiante(int id, String nombre, String apellido, int edad, String facultad) {
         this.id = id;
         this.nombre = nombre;
         this.apellido = apellido;
@@ -16,59 +16,67 @@ public class Estudiante {
         this.facultad = facultad;
     }
 
-    //Metodos get y set ó (getter and setter)
-    public int getId(){
+    // Metodos get y set ó (getter and setter)
+    public int getId() {
         return id;
     }
 
-    public void setId(int id){
+    public void setId(int id) {
         this.id = id;
     }
 
-    public String getNombre(){
+    public String getNombre() {
         return nombre;
     }
 
-    public void setNombre(String nombre){
+    public void setNombre(String nombre) {
         this.nombre = nombre;
     }
 
-    public String getApellido(){
+    public String getApellido() {
         return apellido;
     }
 
-    public void setApellido(String apellido){
+    public void setApellido(String apellido) {
         this.apellido = apellido;
     }
 
-    public int getEdad(){
+    public int getEdad() {
         return edad;
     }
 
-    public void setEdad(int edad){
+    public void setEdad(int edad) {
         this.edad = edad;
     }
 
-    public String getFacultad(){
+    public String getFacultad() {
         return facultad;
     }
 
-    public void setFacultad(String facultad){
+    public void setFacultad(String facultad) {
         this.facultad = facultad;
     }
 
-    //Métodos de la clase
-    public void matricularCursos(String curso){
+    // Métodos de la clase
+    public void matricularCursos(String curso) {
         System.out.println("Curso: " + curso + " Matriculado!!.");
     }
-    
-    public String verificarCreditos(int creditos){
+
+    public String verificarCreditos(int creditos) {
         String cad = "";
-        if(creditos < 10){
+        if (creditos < 10) {
             cad = "No puede matricular menos de 10 créditos";
-        }else{
+        } else {
             cad = "Sus créditos cumplen con lo requerido";
         }
         return cad;
+    }
+
+    public String toString() {
+        return "Estudiante { id:" + id +
+                " nombre:" + nombre +
+                " apellido:" + apellido +
+                " edad:" + edad +
+                " facultad:" + facultad + "}";
     }
 }
