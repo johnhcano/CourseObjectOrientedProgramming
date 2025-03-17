ba85f428d78aa573ec5746d33d0de9f36b2c420f
JhonHCanoB <johnhcano@gmail.com>
2025-03-17 14:03:03 +0000

    Creación de los objetos en la clase Ejecutar

--- 20251 Diurno/Clase7/Ejecutar.java
+++ 20251 Diurno/Clase7/Ejecutar.java
@@ -1,7 +1,10 @@
 public class Ejecutar {
    public static void main(String[] args) {
     
-    
+    //Instanciar el Objeto (Creación)
+    Estudiante objEst1 = new Estudiante(1, "Gabriela", "Astorquiza", 15, "Ingeniería");
+    Estudiante objEst2 = new Estudiante(2, "Juan", "Castro", 20, "Derecho");
+
 
    } 
 }
