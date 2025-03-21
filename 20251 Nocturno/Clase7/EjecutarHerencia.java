9e29e4e5e4a45a27849528fc82a17372674e3980
JhonHCanoB <johnhcano@gmail.com>
2025-03-21 01:03:53 +0000

    Uso del Arreglo de Objetos

--- 20251 Nocturno/Clase7/EjecutarHerencia.java
+++ 20251 Nocturno/Clase7/EjecutarHerencia.java
@@ -2,12 +2,17 @@ public class EjecutarHerencia {
     
     public static void main(String[] args) {
         
-        //Creación de los objetos (Instanciar)
-        Trabajador objT1 = new Operario(1, "Pepito", "Puertas", 135.0);
-        Trabajador objT2 = new Consultor(2, "Juan", "Islas", 10);
+        //Arreglo de trabajadores
+        Trabajador[] t = new Trabajador[3];
 
-        System.out.println(objT1);
-        System.out.println(objT2);
+        //Creación de los objetos (Instanciar)
+        t[0] = new Operario(1, "Pepito", "Puertas", 135.0);
+        t[1] = new Consultor(2, "Juan", "Islas", 10);
+        t[2] = new Consultor(3, "Byron", "Cano", 20);
+
+        for(Trabajador x: t){
+            System.out.println(x.getClass() + " - " + x.pagar());
+        }
 
     }
 }
