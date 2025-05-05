5c3e65e25c7747ea849fa76a4fd844c92b47e9e2
JhonHCanoB <johnhcano@gmail.com>
2025-05-05 12:48:11 +0000

    Creación de la clase Converter

---
+++ 20251 Diurno/Clase12/Converter.java
@@ -0,0 +1,25 @@
+public class Converter {
+    
+    private String document;
+
+    public Converter(String document){
+        this.document = document;
+    }
+
+    public String getDocument(){
+        return document;
+    }
+
+    public void makeLine(){
+        System.out.println("");
+    }
+
+    public void makeParagraph(){
+        System.out.println("");
+    }
+
+    public void makeTable(){
+        System.out.println("");
+    }
+
+}
