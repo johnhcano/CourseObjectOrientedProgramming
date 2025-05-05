40bb77fcb3b9cb1dfd20bd33aba0c820b2171870
JhonHCanoB <johnhcano@gmail.com>
2025-05-05 13:29:20 +0000

    Creación de la clase Reader

---
+++ 20251 Diurno/Clase12/Reader.java
@@ -0,0 +1,31 @@
+public class Reader {
+
+    private String element;
+    private Converter converter;
+
+    public Reader(String element, Converter converter){
+        this.element = element;
+        this.converter = converter;
+    }
+
+    public String getElement() {
+        return element;
+    }
+
+    public void parseInput(){
+        switch(element){
+            case "LINE":
+                converter.makeLine();
+                break;
+            case "PARAGRAPH":
+                converter.makeParagraph();
+                break;
+            case "TABLE":
+                converter.makeTable();
+                break;
+            default:
+                System.out.println("Opción no válida");
+        }
+    }
+
+}
