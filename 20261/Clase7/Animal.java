bf2394849826aa18dc2c952f29514b135a906172
JhonHCanoB <johnhcano@gmail.com>
2026-03-18 01:09:48 +0000

    Clase Animal

--- 20261/Clase7/Animal.java
+++ 20261/Clase7/Animal.java
@@ -1,3 +1,38 @@
 public class Animal {
     
+    private String nombre;
+    private int edad;
+
+    public Animal(String nombre, int edad) {
+        this.nombre = nombre;
+        this.edad = edad;
+    }
+
+    public String getNombre() { 
+        return nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public void comer() {
+        System.out.println(nombre + " está comiendo.");
+    }
+
+    public void hacerSonido() {
+        System.out.println(nombre + " hace un sonido genérico.");
+    }
+
+    public String toString() {
+        return "Animal[ nombre=" + nombre + ", Edad=" + edad + "]";
+    }
 }
