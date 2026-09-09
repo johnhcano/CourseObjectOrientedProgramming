ecc45f4ff52fcacb37fb75b090b00e5fb815dabe
JhonHCanoB <johnhcano@gmail.com>
2026-09-09 00:50:07 +0000

    se cambió el mensaje de bienvenida

--- 2026/poo_2620_7257/Semana1/EjercicioClase1.java
+++ 2026/poo_2620_7257/Semana1/EjercicioClase1.java
@@ -1,5 +1,5 @@
 public class EjercicioClase1 {
     public static void main(String[] args) {
-        System.out.println("Programación Orientada a Objetos");
+        System.out.println("Mi primer programa");
     }    
 }
