9a032bb1debf844c8527a345587685f5f76fdcdf
JhonHCanoB <johnhcano@gmail.com>
2026-09-09 00:47:24 +0000

    Se actualizaron los archivos

---
+++ 2026/poo_2620_7257/Semana1/EjercicioClase1.java
@@ -0,0 +1,5 @@
+public class EjercicioClase1 {
+    public static void main(String[] args) {
+        System.out.println("Programación Orientada a Objetos");
+    }    
+}
--- 2026/poo_2620_7257/Semana2/PrimerEjemplo.java
+++
@@ -1,5 +0,0 @@
-public class PrimerEjemplo {
-    public static void main(String[] args) {
-        System.out.println("Hola");
-    }
-}
