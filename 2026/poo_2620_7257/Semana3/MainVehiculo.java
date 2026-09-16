299724d7557597c855ce88894c771179abf22e1c
JhonHCanoB <johnhcano@gmail.com>
2026-09-16 02:02:31 +0000

    update

--- 2026/poo_2620_7257/Semana3/MainVehiculo.java
+++ 2026/poo_2620_7257/Semana3/MainVehiculo.java
@@ -1,13 +1,16 @@
 public class MainVehiculo {
     public static void main(String[] args) {
         
+        //Una constante es un valor que no cambia durante la ejecución y debe ir 
+        //en Mayúscula Sostenida
+        final int ANIO_ACTUAL = 2026;
+
         Vehiculo objVehiculo1 = new Vehiculo(2025, "FIAT", "Azul", 980.5, "Sedan");
         Vehiculo objVehiculo2 = new Vehiculo(1956, "Chevrolet", "Rojo", 1300.7, "Buick");
         
-        System.out.println(objVehiculo1.calcularAntiguedadVehiculo(2026));
-        System.out.println(objVehiculo2.calcularAntiguedadVehiculo(2026));
-        
-
-
+        System.out.println(objVehiculo1.calcularAntiguedadVehiculo(ANIO_ACTUAL));
+        System.out.println(objVehiculo2.calcularAntiguedadVehiculo(ANIO_ACTUAL));
+       
     }    
+
 }
